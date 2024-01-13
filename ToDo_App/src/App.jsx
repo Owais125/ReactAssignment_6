38b5fa86b80ccb3b437b9d5401b57e2802bac255
Owais_khatri <owaisraza12368@gmail.com>
2024-01-13 12:10:47 -0800

    Work Complete

---
+++ ToDo_App/src/App.jsx
@@ -0,0 +1,20 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+import { Component_1 } from './Components/Component_1';
+
+function App() {
+  return (
+    <>
+
+
+      <Component_1 />
+
+
+
+
+    </>
+  )
+
+}
+
+export default App
